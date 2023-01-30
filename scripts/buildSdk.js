7add6363e993b4d7fcc0ab8bf9b3665e2e239cfb
Sardonyx <canboraciner@gmail.com>
2023-01-31 01:46:29 +0300

    Add version number to SDK CI

--- scripts/buildSdk.js
+++ scripts/buildSdk.js
@@ -1,5 +1,6 @@
 /* eslint-disable */
 const generator = require("openapi-typescript-codegen");
+const fetch = require("node-fetch")
 const fs = require("fs");
 const { execSync } = require("child_process");
 
@@ -14,23 +15,23 @@ const { execSync } = require("child_process");
 
      fs.writeFileSync("./sdk/tsconfig.json", JSON.stringify({
           "compilerOptions": {
-            "module": "commonjs",
-            "declaration": true,
-            "removeComments": true,
-            "emitDecoratorMetadata": true,
-            "experimentalDecorators": true,
-            "allowSyntheticDefaultImports": true,
-            "target": "es2017",
-            "sourceMap": true,
-            "outDir": "./dist",
-            "baseUrl": "./",
-            "incremental": true,
-            "skipLibCheck": true,
-            "strictNullChecks": false,
-            "noImplicitAny": false,
-            "strictBindCallApply": false,
-            "forceConsistentCasingInFileNames": false,
-            "noFallthroughCasesInSwitch": false
+               "module": "commonjs",
+               "declaration": true,
+               "removeComments": true,
+               "emitDecoratorMetadata": true,
+               "experimentalDecorators": true,
+               "allowSyntheticDefaultImports": true,
+               "target": "es2017",
+               "sourceMap": true,
+               "outDir": "./dist",
+               "baseUrl": "./",
+               "incremental": true,
+               "skipLibCheck": true,
+               "strictNullChecks": false,
+               "noImplicitAny": false,
+               "strictBindCallApply": false,
+               "forceConsistentCasingInFileNames": false,
+               "noFallthroughCasesInSwitch": false
           }
      }))
 
@@ -42,7 +43,8 @@ const { execSync } = require("child_process");
           name: "musician-app-sdk",
           main: "./dist/index.js",
           files: ["dist/*"],
-          license: "UNLICENSED"
+          license: "UNLICENSED",
+          version: require("../package.json").version
      }));
 
      fs.copyFileSync("./assets/README.sdk.md", "./sdk/README.md");
