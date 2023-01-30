c3ce84354e35f2a97d1a01284aa0879c75ff79cb
Sardonyx <canboraciner@gmail.com>
2023-01-31 01:32:34 +0300

    Better naming conventions

--- src/decorators/api-requires-auth.ts
+++ src/decorators/api-requires-auth.ts
@@ -2,7 +2,7 @@ import { UseGuards } from "@nestjs/common";
 import { ApiBearerAuth, ApiUnauthorizedResponse } from "@nestjs/swagger";
 import { AuthGuard } from "src/auth/auth.guard";
 
-export function ApiAuthRequires() {
+export function ApiRequiresAuth() {
      return function (target) {
           ApiBearerAuth("defaultJWT")(target);
           ApiUnauthorizedResponse({ description: "Authorization header is missing or invalid" })(target);
